7354afbe986b39b59e77b811a707794e6603dd60
nayhub <antguajardo2021@udec.cl>
2023-11-30 18:43:04 -0300

    feat: creación de la clase InferfazGUI

---
+++ src/main/java/InterfazGUI.java
@@ -0,0 +1,14 @@
+/**
+ * Clase que representa la interfaz gráfica de usuario.
+ */
+public class InterfazGUI {
+    /**
+     * Método principal de la aplicación.
+     *
+     * @param args Argumentos de línea de comandos.
+     */
+    public static void main(String[] args) {
+        VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+        ventanaPrincipal.setVisible(true);
+    }
+}
